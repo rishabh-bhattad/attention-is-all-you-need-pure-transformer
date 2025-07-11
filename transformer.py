cb819143d197b30b9ceb445275687db08f4e4350
rishabh <rishabh.bhattad@gmail.com>
2025-07-10 22:39:14 -0700

    Transformer block

--- transformer.py
+++ transformer.py
@@ -35,5 +35,36 @@ class SelfAttention(nn.Module):
         if mask is not None:
             energy = energy.masked_fill(mask == 0, float("-1e20"))
 
+        attention = torch.softmax(energy / self.embed_size ** (1/2), dim=3)
+        out = torch.einsum("nhql, nlhd->nqhd", [attention, values]).reshape(
+            N, query_len, self.heads*self.head_dim
+        )
+        # attention shape: (n, heads, query_len, key_len)
+        # Values shape: (n, values_len, heads, heads_dim)
+        # After einsum -> (N, query_len, heads, heads_dim)
+
+        out = self.fc_out(out)
+        return out
 
 
+class TransformerBlock(nn.Module):
+    def __init__(self, embed_size, heads, dropout, forward_expansion):
+        super().__init__()
+        self.attention = SelfAttention(embed_size, heads)
+        self.norm1 = nn.LayerNorm(embed_size)
+        self.norm2 = nn.LayerNorm(embed_size)
+
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embed_size, forward_expansion*embed_size),
+            nn.ReLU(),
+            nn.Linear(forward_expansion*embed_size, embed_size)
+        )
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, value, key, query, mask):
+        attention = self.attention(value, key, query, mask)
+
+        x = self.dropout(self.norm1(attention + query))
+        forward = self.feed_forward(x)
+        out = self.dropout(self.norm2(forward + x))
+        return out
